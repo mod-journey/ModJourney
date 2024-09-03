569d0b14b6fb9e259498916b566043665762faf3
zukkl <mrozikrobin@gmail.com>
2024-09-03 12:29:46 +0200

    Cherry-Pick custom quests
        - picked from dytech-4@d5f96369 and dytech-4@7facb845

---
+++ kubejs/server_scripts/ftbquests/daily_two.js
@@ -0,0 +1,43 @@
+//priority 10
+
+const $FTBTeamsAPI = Java.loadClass("dev.ftb.mods.ftbteams.api.FTBTeamsAPI");
+
+FTBQuestsEvents.customTask('51300044341BFFCC', event => {
+    event.maxProgress = 1440 // 4h max
+
+    event.setCheckTimer(18000) // Timer wird jede  Minuten geprueft
+
+    event.setCheck((task) => {
+        task.progress + 15 // Adds progress to the quest.
+    })
+})
+
+FTBQuestsEvents.customReward("65A83C70BA4FDE02", event => {
+
+    const teamManager = $FTBTeamsAPI.api().getManager();
+    const playerUUID = event.player.getUuid();
+    const teamOptional = teamManager.getTeamForPlayerID(playerUUID);
+
+    if (teamOptional.isPresent()) {
+        const team = teamOptional.get();
+
+        // Mitglieder des Teams abrufen
+        const teamMembers = team.getMembers();
+
+        // Anzahl der Mitglieder im Team
+        const memberCount = teamMembers.size();
+
+        // Anzahl der Teammitglieder ausgeben
+        console.log("Quest-Erfolg: " + event.player.name.getString() + " hat erfolgreich " + memberCount + " Münze/n für sein Team abgeholt.");
+        event.player.tell("Du hast erfolgreich " + memberCount + " Münze/n für dein Team abgeholt")
+
+        for (let n = 1; n <= memberCount; n++) {
+
+            event.player.give("mod_journey:gold_coin")
+        }
+    } else {
+        console.log("Quest-Fehler: " + event.player.name.getString() + " hat nur einen Goldcoin erhalten")
+        event.player.tell("Etwas ist bei der Abgabe der Quest schiefgelaufen, bitte Kontaktiere umgehend die Orga.")
+        event.player.give("mod_journey:gold_coin")
+    }
+})
