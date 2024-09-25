5dc1e5791a0416d6c8ae3bb8fcde7c53706b5907
zukkl <mrozikrobin@gmail.com>
2024-09-25 12:20:41 +0200

    add logic to daily quest team reward

--- kubejs/server_scripts/ftbquests/daily_two.js
+++ kubejs/server_scripts/ftbquests/daily_two.js
@@ -1,11 +1,31 @@
-//priority 10
+const $FTBTeamsAPI = Java.loadClass("dev.ftb.mods.ftbteams.api.FTBTeamsAPI");
 
-FTBQuestsEvents.customTask('51300044341BFFCC', event => {
-    event.maxProgress = 1440 // 4h max
+FTBQuestsEvents.customReward("65A83C70BA4FDE02", event => {
 
-    event.setCheckTimer(18000) // Timer wird jede  Minuten geprueft
+    const teamManager = $FTBTeamsAPI.api().getManager();
+    const playerUUID = event.player.getUuid();
+    const teamOptional = teamManager.getTeamForPlayerID(playerUUID);
 
-    event.setCheck((task) => {
-        task.progress + 15 // Adds progress to the quest.
-    })
+    if (teamOptional.isPresent()) {
+        const team = teamOptional.get();
+
+        // Mitglieder des Teams abrufen
+        const teamMembers = team.getMembers();
+
+        // Anzahl der Mitglieder im Team
+        const memberCount = teamMembers.size();
+
+        // Anzahl der Teammitglieder ausgeben
+        console.log("Quest-Erfolg: " + event.player.name.getString() + " hat erfolgreich " + memberCount + " Münze/n für sein Team abgeholt.");
+        event.player.tell("Du hast erfolgreich " + memberCount + " Münze/n für dein Team abgeholt")
+
+        for (let n = 1; n <= memberCount; n++) {
+
+            event.player.give("thermal:gold_coin")
+        }
+    } else {
+        console.log("Quest-Fehler: " + event.player.name.getString() + " hat nur einen Goldcoin erhalten")
+        event.player.tell("Etwas ist bei der Abgabe der Quest schiefgelaufen, bitte Kontaktiere umgehend die Orga.")
+        event.player.give("thermal:gold_coin")
+    }
 })
