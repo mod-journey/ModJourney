059f5cce52a4d51f2f0605353a7e6711a32f285f
Radon8472 <develop@radon-software.net>
2024-10-13 18:14:37 +0200

    #158 Rework to loop over all time ranks

--- kubejs/server_scripts/observe/player_ranks.js
+++ kubejs/server_scripts/observe/player_ranks.js
@@ -1,16 +1,27 @@
+const ftbTimeRanks = [
+    'weekling',
+    'double-weekling',
+    'triple-weekling',
+    'monthling'
+]
+
+/**
+ * Checks if player are missing any of the time ranks and add them
+ */
 PlayerEvents.loggedIn(e => {
     e.entity.tell(`Welcome ${e.entity.name.string}`);
 
-    const rank = 'weekling'; // double-weekling, triple-weekling, monthling
+    ftbTimeRanks.forEach(rank => {
 
+        // run if player does not have the rang
+        if ( !e.entity.getTags().contains(rank) ) {
+            Utils.server.runCommandSilent(`ftbranks add ${e.entity.name.string} ${rank}`);
 
-    // run if player does not have the rang
-    if ( !e.entity.getTags().contains(rank) ) {
-        Utils.server.runCommandSilent(`ftbranks add ${e.entity.name.string} ${rank}`);
+            // Utils.server.runCommandSilent(`tag ${e.entity.name.string} add ${rang}`)
+            e.entity.addTag(rank)
+            console.log(`Added rank ${rank} to player ${e.entity.name.string}`);
+        }
 
-        // Utils.server.runCommandSilent(`tag ${e.entity.name.string} add ${rang}`)
-        e.entity.addTag(rank)
-        console.log(`Added rank ${rank} to player ${e.entity.name.string}`);
-    }
+    })
 
 })
