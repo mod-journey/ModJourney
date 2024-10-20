6d8f5a14768afb1f1c902687726241c6f6003241
zukkl <mrozikrobin@gmail.com>
2024-10-20 16:30:43 +0200

    #111, WIP: try to source out code to case.

--- kubejs/server_scripts/observe/click_listener/ranks.js
+++ kubejs/server_scripts/observe/click_listener/ranks.js
@@ -2,6 +2,8 @@ ItemEvents.rightClicked("minecraft:stick", event => {
     if (!mod_jounrey_server_check(event)) return;
     if (event.player.stages.has("member")) return;
 
+    let controll_rank = true;
+
     /**@param {String} search_tag  search for a player tag.*/
     function player_tag(search_tag) {
         return event.player.stages.has(search_tag);
@@ -31,28 +33,78 @@ ItemEvents.rightClicked("minecraft:stick", event => {
       if some admin rank was found, the player and server will notify, that the player got his no_work rank.
       also a tag will add to player. this is for the first setup.
     */
-    if (!(search_for_tag("orga_kein_dienst") || search_for_tag("support_kein_dienst") || search_for_tag("admin_kein_dienst"))) {
-        if (!search_for_tag(administrative_ranks)) {
-            event.player.stages.add("member")
-            console.log(event.player.name.getString() + " hat den Tag \"member\" verliehen bekommen.")
-            event.player.tell("§3" + event.player.name.getString() + "§r" + " hat den Tag §3member§r verliehen bekommen.")
-        }
-        else if (search_for_tag(administrative_ranks) === "orga_rang") {
-            event.player.stages.add("orga_kein_dienst")
-            console.log(event.player.name.getString() + " hat den Tag \"orga_kein_dienst\" verliehen bekommen.")
-            event.player.tell("§3" + event.player.name.getString() + "§r" + " hat den Tag §3orga_kein_dienst§r verliehen bekommen.")
-        }
-        else if (search_for_tag(administrative_ranks) === "support_rang") {
-            event.player.stages.add("support_kein_dienst")
-            console.log(event.player.name.getString() + " hat den Tag \"support_kein_dienst\" verliehen bekommen.")
-            event.player.tell("§3" + event.player.name.getString() + "§r" + " hat den Tag §3support_kein_dienst§r verliehen bekommen.")
-        }
-        else if (search_for_tag(administrative_ranks) === "admin_rang") {
-            event.player.stages.add("admin_kein_dienst")
-            console.log(event.player.name.getString() + " hat den Tag \"admin_kein_dienst\" verliehen bekommen.")
-            event.player.tell("§3" + event.player.name.getString() + "§r" + " hat den Tag §3admin_kein_dienst§r verliehen bekommen.")
+    if (!search_for_tag(administrative_ranks)) {
+        event.player.stages.add("member")
+        console.log(event.player.name.getString() + " hat den Tag \"member\" verliehen bekommen.")
+        event.player.tell("§3" + event.player.name.getString() + "§r" + " hat den Tag §3member§r verliehen bekommen.")
+        controll_rank = false
+    }
+    else if (search_for_tag(administrative_ranks) === "orga_rang" && !player_tag("orga_kein_dienst")) {
+        event.player.stages.add("orga_kein_dienst")
+        console.log(event.player.name.getString() + " hat den Tag \"orga_kein_dienst\" verliehen bekommen.")
+        event.player.tell("§3" + event.player.name.getString() + "§r" + " hat den Tag §3orga_kein_dienst§r verliehen bekommen.")
+        controll_rank = false
+    }
+    else if (search_for_tag(administrative_ranks) === "support_rang" && !player_tag("support_kein_dienst")) {
+        event.player.stages.add("support_kein_dienst")
+        console.log(event.player.name.getString() + " hat den Tag \"support_kein_dienst\" verliehen bekommen.")
+        event.player.tell("§3" + event.player.name.getString() + "§r" + " hat den Tag §3support_kein_dienst§r verliehen bekommen.")
+        controll_rank = false
+    }
+    else if (search_for_tag(administrative_ranks) === "admin_rang" && !player_tag("admin_kein_dienst")) {
+        event.player.stages.add("admin_kein_dienst")
+        console.log(event.player.name.getString() + " hat den Tag \"admin_kein_dienst\" verliehen bekommen.")
+        event.player.tell("§3" + event.player.name.getString() + "§r" + " hat den Tag §3admin_kein_dienst§r verliehen bekommen.")
+        controll_rank = false
+    }
+
+    /**
+     * @param {String} old_tag The Player Tag, which will looking for.
+     * @param {String} new_tag The Player Tag, which will add now.
+     * @param {Int} status
+     */
+    function promote_player_to_active(old_tag, new_tag, status) {
+        //event.player.tell(player_tag(new_tag.slice("_")[0]))
+        //event.player.tell(new_tag.concat("_rang"))
+        if (controll_rank) {
+            if (player_tag(old_tag)) {
+                //Feste Commands
+                event.server.runCommandSilent(`tag ${event.player.name.getString()} add ${new_tag}`)
+                event.server.runCommandSilent(`tag ${event.player.name.getString()} remove ${old_tag}`)
+                console.log("1")
+
+                //Logging
+                event.player.tell(`Du hast deinen Rang gewechselt von §3${old_tag}§r zu §3${new_tag}§r.`)
+                console.log(`"${event.player.name.getString()}" hat seinen Rang gewechselt von "${old_tag}" zu "${new_tag}"`)
+
+                if (status === 1) { //wenn Spieler beitritt "Orga aktiv"
+                    event.server.runCommandSilent(`team join ${new_tag.split("_")[0]} ${event.player.name.getString()}`)
+                    console.log(` ${event.player.name.getString()} ist nun als ${new_tag.split("_")[0]} aktiv.`)
+                    console.log("2")
+
+                }
+                else if (status === 2) { //wenn Spieler beitritt "Orga mit rechten"
+                    event.server.runCommand(`luckperms user ${event.player.name.getString()} promote`)
+                    console.log(`${event.player.name.getString()} hat nun erweiterte Rechte.`)
+                    console.log("3")
+
+                }
+                else if (status === 3) { //wenn Spieler beitritt "Orga inaktiv"
+                    event.player.tell("code")
+                    event.player.runCommandSilent(`team leave ${event.player.name.getString()}`)
+                    console.log("4")
+
+                }
+                console.log("5")
+                return controll_rank = false
+            }
         }
     }
+
+    promote_player_to_active("admin_kein_dienst", "admin_im_dienst", 1)
+    promote_player_to_active("admin_im_dienst", "admin_mit_rechten", 2)
+    promote_player_to_active("admin_mit_rechten", "admin_kein_dienst", 3)
+
 })
 
 
