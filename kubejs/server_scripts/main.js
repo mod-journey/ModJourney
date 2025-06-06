891a2461451692f14a871644b95ad7f0235e41c0
zukkl <mrozikrobin@gmail.com>
2025-06-06 03:28:41 +0200

    add functions to Main File

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -8,6 +8,7 @@
  *  botania_recipe_gated(event)
  */
 ServerEvents.recipes((event) => {
+    /* "./gated/* */
     replacer.init(event);
 
     adastra_gated( event );
@@ -33,4 +34,24 @@ ServerEvents.recipes((event) => {
     thermal_gated( event );
 
     remove_item_by_tag( event );    //Checking scipt of using
+
+
+    /* "./add" */
+    add_adastra_items( event );
+    add_ae2_fluix( event );
+    add_ae2_skystellingot( event );
+    add_creativeitemsMagic( event );
+    add_creativeitemsTechnic( event );
+    add_immersive_alternativeCircuitRecipes ( event );
+    add_immersive_excavator( event );
+    //add_industrial_laser_fluidLaser( event ); --> actually no content in this file.
+    add_magic_bloodMagic( event );
+    add_magic_botania( event );
+    add_magic_botanyPots( event );
+    add_modjourney_antimatter( event );
+    add_modjourney_bloodmagic( event );
+    add_modjourney_scrolls( event );
+    // ./add/mod_journey/tag_scrolls.js ---> Tag Event auf Items ggf. verschieben in tags im Zukünftigen Kommit. >"tag_event_mj_scrolls( event );"<
+    add_cataclysm( event );
+    add_modjourneycoins( event );
 })
