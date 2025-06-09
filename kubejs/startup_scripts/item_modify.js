54824691c9cea154b218319dfe8d654f701a34aa
zukkl <mrozikrobin@gmail.com>
2025-06-09 03:01:59 +0200

    merge startup code, and commit them out

--- kubejs/startup_scripts/item_modify.js
+++ kubejs/startup_scripts/item_modify.js
@@ -1,16 +1,16 @@
 // priority: 0
 
-console.info('Hello, World! (Loaded startup scripts)')
 
-ItemEvents.modification(event => {
+let item_modify = event => {
 
-    event.modify('mob_grinding_utils:saw_upgrade_looting', item => {
-      item.maxStackSize = 3
-      item.fireResistant = true
-    })
-    
-    event.modify('mob_grinding_utils:saw_upgrade_beheading', item => {
-        item.maxStackSize = 1
-        item.fireResistant = true
-    })
-})
+  event.modify('mob_grinding_utils:saw_upgrade_looting', item => {
+    item.maxStackSize = 3
+    item.fireResistant = true
+  })
+
+  event.modify('mob_grinding_utils:saw_upgrade_beheading', item => {
+    item.maxStackSize = 1
+    item.fireResistant = true
+  })
+
+};
