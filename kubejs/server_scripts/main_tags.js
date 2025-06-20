e48719e051270f3698953bc5ea2333a436e91b8e
zukkl <mrozikrobin@gmail.com>
2025-06-20 22:44:08 +0200

    move tag events in main_tags

---
+++ kubejs/server_scripts/main_tags.js
@@ -0,0 +1,17 @@
+// priority: 100
+
+let main_tags = false;
+
+while (main_tags) {
+    ServerEvents.tags('item', event => {
+
+        ores_ad_astra_tag(event)
+        //ores_meka_tag(event)
+        ores_immersive_tag(event)
+        ores_thermal_tag(event)
+        ores_industrial_tag(event)
+        thermal_casts(event)
+        ato_tags(event)
+
+    })
+}
