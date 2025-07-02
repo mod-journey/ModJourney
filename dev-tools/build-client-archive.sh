c6a1b4c335efa17c24b1989b5d020ddbff9371c4
Radon8472 <develop@radon-software.net>
2025-07-02 23:21:32 +0200

    Fix path variables in build script

--- dev-tools/build-client-archive.sh
+++ dev-tools/build-client-archive.sh
@@ -9,11 +9,12 @@
 
 echo "Building client archive"
 File_Mode="client"
-SCRIPT_DIR="$(dirname "$0")"
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 PROJECT_DIR="${SCRIPT_DIR}/../"
-Version_Number="$(cat ${SCRIPT_DIR}/../manifest.json | head -n 14 | tail -n 1 | cut -d '"' -f 4)"
-ARCHIVE_FILE_NAME=mod-journey-${File_Mode}-${Version_Number}.zip
+Version_Number="$(cat "${PROJECT_DIR}/manifest.json" | head -n 14 | tail -n 1 | cut -d '"' -f 4)"
+ARCHIVE_FILE_NAME=mod-journey-2-${File_Mode}-${Version_Number}.zip
 BUILD_DIR=$(realpath "${PROJECT_DIR}build/")
+mkdir -p "${SCRIPT_DIR}/logs/"
 LOG_FILE="${SCRIPT_DIR}/logs/$(date +'%Y-%m-%d_%H-%M-%S').log"
 exec > >(tee -a "$LOG_FILE") 2>&1
 
