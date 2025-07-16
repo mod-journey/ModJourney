e5cac1d301e98cf16c812fdf398b0c07984b4996
zukkl <mrozikrobin@gmail.com>
2025-07-16 20:37:21 +0200

    add and clean up main_startup

--- kubejs/startup_scripts/_main_startup.js
+++ kubejs/startup_scripts/_main_startup.js
@@ -1,18 +1,22 @@
 //priority: 100
 
-let startup = false
+let startup = true
 
 if (startup) {
     ItemEvents.modification(event => {
-        item_modify(event);
+        item_modify_mobgrinding(event, true);
+        item_modify_stellaris(event, true);
+        item_modify_custommachinary(event, true);
+    });
 
+    StartupEvents.registry('block', event => {
+        block_add(event, true);
+    });
+
+    StartupEvents.registry('item', event => {
+        item_add(event, true);
     });
 };
 
-StartupEvents.registry('block', event => {
-    block_add(event, true);
-});
 
-StartupEvents.registry('item', event => {
-    item_add(event, true);
-});
+
