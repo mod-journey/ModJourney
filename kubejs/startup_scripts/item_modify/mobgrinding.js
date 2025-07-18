b6b000c57d129d1417d1da35a62e41d64df8959b
zukkl <mrozikrobin@gmail.com>
2025-07-18 22:13:07 +0200

    remove fire resistence modify from item, that broke game atm

--- kubejs/startup_scripts/item_modify/mobgrinding.js
+++ kubejs/startup_scripts/item_modify/mobgrinding.js
@@ -5,12 +5,10 @@ let item_modify_mobgrinding = (event, active, debug) => {
 
     event.modify('mob_grinding_utils:saw_upgrade_looting', item => {
         item.maxStackSize = 3
-        item.fireResistant = true
     })
 
     event.modify('mob_grinding_utils:saw_upgrade_beheading', item => {
         item.maxStackSize = 1
-        item.fireResistant = true
     })
 
 };
