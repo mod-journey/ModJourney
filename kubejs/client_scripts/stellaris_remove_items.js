b10dcefb3275f418e511b2d5fe09ece4bcdd4bfc
zukkl <mrozikrobin@gmail.com>
2025-07-19 19:43:25 +0200

    #12, Stellaris - remove cables, pumpjack and pipes from JEI

--- kubejs/client_scripts/stellaris_remove_items.js
+++ kubejs/client_scripts/stellaris_remove_items.js
@@ -9,14 +9,24 @@ let stellaris_remove_items = (event, active, debug) => {
         'stellaris:deepslate_steel_ore',
         'stellaris:moon_steel_ore',
         'stellaris:raw_steel_block',
-        'stellaris:steel_block'
+        'stellaris:steel_block',
+        'stellaris:cable',
+        'stellaris:cable_t2',
+        'stellaris:cable_t3',
+        'stellaris:pipe_t1',
+        'stellaris:pipe_t2',
+        'stellaris:pipe_t3',
+        'stellaris:pumpjack',
+        'stellaris:pumpjack_drill',
+        'stellaris:oil_finder'
     ]
 
     steel.forEach(remove_steel => {
         event.remove(remove_steel)
     });
 
-    let uran = ['stellaris:raw_uranium_block',
+    let uran = [
+        'stellaris:raw_uranium_block',
         'stellaris:uranium_block',
         'stellaris:mercury_uranium_ore'
     ]
