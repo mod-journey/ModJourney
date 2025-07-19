96efa6655cb8dfc80b32a7c702248659178c2341
zukkl <mrozikrobin@gmail.com>
2025-07-19 13:50:40 +0200

    motor upgrades removed

--- kubejs/server_scripts/gated/stellaris.js
+++ kubejs/server_scripts/gated/stellaris.js
@@ -7,8 +7,13 @@ let stellaris_gated = (event, active, debug) => {
         'stellaris:radioactive_generator'
     ]
 
+    let motor = ['stellaris:hydrogen_motor', 'stellaris:radioactive_motor']
+
     energy_gen.forEach(staged => {
-        event.replaceInput({output: staged},"minecraft:lapis_lazuli", stages.stage1.core)
+        event.replaceInput({ output: staged }, "minecraft:lapis_lazuli", stages.stage1.core)
     });
+
+    event.remove({ id: "stellaris:misc/upgrades/radioactive_motor" })
+    event.remove({ id: "stellaris:misc/upgrades/hydrogen_motor" })
 
 }
