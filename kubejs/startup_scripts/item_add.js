23d4925f41884f494ac85132e76e43e5927f1afe
Radon8472 <develop@radon-software.net>
2025-07-27 09:37:38 +0200

    Add translation for custom items

--- kubejs/startup_scripts/item_add.js
+++ kubejs/startup_scripts/item_add.js
@@ -1,36 +1,30 @@
 // priority: 90
-console.info('Add custom Scrolls.')
-
 let item_add = (event, active, debug) => {
 
     if (!active) return;
+    if (debug) console.info('Add custom Scrolls.')
 
     for (let i = 1; i <= 9; i++) {
         event.create('mod_journey:scroll' + i)
             .texture('mod_journey:item/scroll' + i)
             .maxStackSize(16)
-            .displayName("Schriftrolle " + i)
             .rarity("EPIC")
     }
 
     event.create('mod_journey:blank_scroll')
         .texture('mod_journey:item/blank_scroll')
         .maxStackSize(64)
-        .displayName("Leere Schriftrolle")
         .rarity("RARE")
 
     event.create('mod_journey:the_rod_of_real_life')
-        .displayName('The Rod of Real Life')
         .rarity("RARE")
         .maxStackSize(1)
 
     event.create('mod_journey:antimatter_ingot')
-        .displayName('Antimatter Ingot')
         .rarity("EPIC")
         .maxStackSize(64)
 
     event.create('mod_journey:clotted_blood_clump')
-        .displayName('Clotted Blood Clump')
         .maxStackSize(64)
 
     let magic_scrolls_array = [
@@ -45,7 +39,6 @@ let item_add = (event, active, debug) => {
         event.create('mod_journey:magic_scroll_' + scroll)
             .texture('mod_journey:magic_scroll_' + scroll)
             .maxStackSize(16)
-            .displayName("Magische Schriftrolle " + magic_counter)
             .rarity("EPIC")
 
         magic_counter = magic_counter + 1;
