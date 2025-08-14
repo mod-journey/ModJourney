289f2e6ab3c3d44afd31b07e49836b77239449d9
zukkl <mrozikrobin@gmail.com>
2025-08-14 10:56:30 +0200

    #109 #99 - MobGrinding now fully staged with global stage object

--- kubejs/server_scripts/main_recipes.js
+++ kubejs/server_scripts/main_recipes.js
@@ -44,7 +44,7 @@ if (events_server) {
             itemcollector_gated(event, true);
             laserio_gated(event, true);
             mekanism_gated(event, true);
-            mobgrindingutils(event, false);                     //much items like weapons and armor must replace, if we have new magic mods.
+            mobgrindingutils(event, true);                     //much items like weapons and armor must replace, if we have new magic mods.
             mysticalagriculture_gated(event, true);
             powah_gated(event, true);
             rfttools_gated(event, true);
