9fb8c194cb1146b81f7986d8dbbfda4d3332f38a
Radon8472 <develop@radon-software.net>
2025-08-16 10:22:53 +0200

    Wrap TaskListener in static QuestBuilder class

--- kubejs/server_scripts/helpers/quest_builder.js
+++ kubejs/server_scripts/helpers/quest_builder.js
@@ -1,34 +1,47 @@
 // priority: 101
 
-
-/**
- * @constructor for TaskListener
- *
- * @param {string} taskID
- */
-function TaskListener(taskID) {
-    this.taskID = taskID
-}
-
-TaskListener.prototype = {
+let QuestBuilder = {
     /**
-     * @param {Number} maxProgress
+     *
+     * @param {string} taskID Hexadecimal ID of the task in questbook
      * @returns {TaskListener}
      */
-    setMaxProgress: function(maxProgress)  {
-        this.maxProgress = maxProgress
-        return this;
+    loadTask: (taskID) => {
+
+        /**
+         * @constructor for TaskListener
+         *
+         * @param {string} taskID
+         */
+        function TaskListener(taskID) {
+            this.taskID = taskID
+            this.setMaxProgress(2)
+        }
+
+        TaskListener.prototype = {
+            /**
+             * @param {Number} maxProgress
+             * @returns {TaskListener}
+             */
+            setMaxProgress: function(maxProgress)  {
+                this.maxProgress = maxProgress
+                return this;
+            },
+
+
+            /**
+             * Build FTBQuests task listener event
+             */
+            build: function() {
+                console.debug(`Register task ${this.taskID} with max ${this.maxProgress}`)
+                FTBQuestsEvents.customTask(taskID, event => {
+                    event.maxProgress = this.maxProgress
+                })
+            }
+        }
+
+        return new TaskListener(taskID)
     },
 
-
-    /**
-     * Build FTBQuests task listener event
-     */
-    build: function() {
-        console.debug(`Register task ${this.taskID} with max ${this.maxProgress}`)
-        FTBQuestsEvents.customTask(taskID, event => {
-            event.maxProgress = this.maxProgress
-        })
-    }
+    // loadReward: TODO implement this
 }
-
