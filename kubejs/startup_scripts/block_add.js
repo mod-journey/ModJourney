5375a3a0a2b69e5b43c18222b39393c14f2a40ed
zukkl <mrozikrobin@gmail.com>
2025-09-04 11:59:10 +0200

    #54 - add primitive Framed Blocks

--- kubejs/startup_scripts/block_add.js
+++ kubejs/startup_scripts/block_add.js
@@ -101,4 +101,18 @@ let block_add = (event, active, debug) => {
         .tagBlock("minecraft:needs_diamond_tool")
         .tagBlock("mekanism:atomic_disassembler_ore")
         .tagBlock("immersiveengineering:mineable/drill")
+
+/*
+    TODO: Durch keine Solid Texture, muss das Rendering geändert werden.
+    Weitere Tags müssten noch vergeben werden!
+*/
+
+    let staged_items = 4;
+    let number_as_word = ["one", "two", "three", "four"];
+
+    for (let i = 1; i <= staged_items; i++) {
+        event.create(`mod_journey:staging/frame_core_${i}`)
+            .tag("mod_journey:staged_item")
+            .tag(`mod_journey:stage/${number_as_word[i - 1]}`)
+    };
 };
