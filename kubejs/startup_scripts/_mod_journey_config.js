ad861ffc0d985f3f17501f70b418a5cbf6cab82f
Radon8472 <develop@radon-software.net>
2025-10-13 23:22:00 +0200

    Move stages to global config

--- kubejs/startup_scripts/_mod_journey_config.js
+++ kubejs/startup_scripts/_mod_journey_config.js
@@ -3,7 +3,73 @@
 let WEEK = HOUR*24*7
 
 global.mjConfig = {
-    stages: 'stages',
+    stages: {
+        stage1: {
+            vanilla: "mod_journey:staging/frame_core_1",
+            core: 'mod_journey:staging/frame_core_2',
+            second: '',
+            endgame: '',
+            magic: {
+                early: '#mod_journey:magic/stage/one/early',
+                mid: 'naturesaura:birth_spirit',
+                end: 'naturesaura:sky_ingot'
+            },
+        },
+        stage2: {
+            core: 'mod_journey:staging/frame_core_3',
+            // indirect gated via darksteel and ender io allow smelting
+            gear: 'enderio:dark_bimetal_gear',
+            second: '',
+            endgame: '',
+            magic: {
+                early: 'occultism:crushed_end_stone',
+                mid: 'occultism:soul_gem',
+                end: 'occultism:chalk_void',
+            },
+        },
+        stage3: {
+            core: 'mod_journey:staging/frame_core_4',
+            second: '',
+            endgame: '',
+            magic: {
+                early: 'ars_nouveau:source_gem',
+                mid: 'ars_nouveau:archmage_spell_book',
+                end: 'apothic_enchanting:ender_library'
+            },
+        },
+        mod_based: {
+            mekanism: "mekanism:steel_casing",
+            mobGrindingUtils: {
+                stick: '#c:rods/iridium',
+                swords: [
+                    'naturesaura:depth_sword',
+                    'naturesaura:infused_iron_sword'
+                ],
+                helmets: [
+                    'naturesaura:depth_helmet',
+                    'naturesaura:infused_iron_helmet'
+                ],
+                chestplates: [
+                    'naturesaura:depth_chest',
+                    'naturesaura:infused_iron_chest'
+                ],
+                pents: [
+                    'naturesaura:depth_pants',
+                    'naturesaura:infused_iron_pants'
+                ],
+                shoes: [
+                    'naturesaura:depth_shoes',
+                    'naturesaura:infused_iron_shoes'
+                ]
+            }
+        },
+        coins: {
+            bronze: "mod_journey:copper_coin",
+            silver: "mod_journey:silver_coin",
+            gold: "mod_journey:gold_coin"
+        }
+    },
+
     times: {
         projectStart: new Date(2025, 9, 31, 18, 0).getTime() / 1000
     }
