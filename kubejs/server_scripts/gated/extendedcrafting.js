ddd56bac90394dd0e248494588a7ac6e8e94a2d9
zukkl <mrozikrobin@gmail.com>
2025-10-13 14:18:53 +0200

    change extended Crafting Staging to Craftbenchs, to make it more balance

--- kubejs/server_scripts/gated/extendedcrafting.js
+++ kubejs/server_scripts/gated/extendedcrafting.js
@@ -2,10 +2,34 @@
 
 let extendedcrafting_gated = (event, active, debug) => {
     if (!active) return;
-    // Void Chassis
+
     event.replaceInput(
-        { id: 'extendedcrafting:black_iron_slate' },
-        'extendedcrafting:black_iron_ingot',
+        {id: 'extendedcrafting:basic_table'},
+        'minecraft:iron_block',
+        stages.stage3.core
+    )
+
+    event.replaceInput(
+        {id: 'extendedcrafting:compressor'},
+        'extendedcrafting:black_iron_slate',
+        stages.stage3.core
+    )
+
+    event.replaceInput(
+        {id: 'extendedcrafting:crafting_core'},
+        'extendedcrafting:black_iron_slate',
+        stages.stage3.core
+    )
+
+    event.replaceInput(
+        {id: 'extendedcrafting:flux_crafter'},
+        'minecraft:crafting_table',
+        stages.stage3.core
+    )
+
+    event.replaceInput(
+        {id: 'extendedcrafting:ender_crafter'},
+        'minecraft:crafting_table',
         stages.stage3.core
     )
 }
