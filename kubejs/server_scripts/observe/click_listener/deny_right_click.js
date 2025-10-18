97df3a1fdcd650c2e5fb82340b29e47414ea113e
zukkl <mrozikrobin@gmail.com>
2025-10-18 13:22:16 +0200

    #122 - add Interact event to deny changing other entitys.

--- kubejs/server_scripts/observe/click_listener/deny_right_click.js
+++ kubejs/server_scripts/observe/click_listener/deny_right_click.js
@@ -1,3 +1,8 @@
 ItemEvents.rightClicked("stellaris:oil_bucket", event => {
     event.cancel()
 })
+
+// Don't allow to change size of other entitys
+ItemEvents.entityInteracted(Item.of('shrink:shrinking_device'), event => {
+    event.cancel()
+})
