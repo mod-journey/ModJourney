aa23831afc641764a85e11d8bfa741899ae8eadb
zukkl <mrozikrobin@gmail.com>
2025-10-22 21:29:20 +0200

    change immersive modls stackable to 3

--- kubejs/startup_scripts/item_modify/custom_machinary.js
+++ kubejs/startup_scripts/item_modify/custom_machinary.js
@@ -6,6 +6,15 @@ let item_modify_custommachinary = (event, active, debug) => {
     event.modify("immersiveengineering:mold_gear", item => {
         item.maxStackSize = 3
     })
+    event.modify("immersiveengineering:mold_wire", item => {
+        item.maxStackSize = 3
+    })
+    event.modify("immersiveengineering:mold_plate", item => {
+        item.maxStackSize = 3
+    })
+    event.modify("immersiveengineering:mold_rod", item => {
+        item.maxStackSize = 3
+    })
 
 }
 
