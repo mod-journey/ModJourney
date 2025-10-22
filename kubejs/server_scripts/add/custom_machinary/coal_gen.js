048cb3e46e5ab49fca1151b37c6d3c93fa91fdd3
zukkl <mrozikrobin@gmail.com>
2025-10-22 21:28:12 +0200

    remove coal and water gen

--- kubejs/server_scripts/add/custom_machinary/coal_gen.js
+++
@@ -1,7 +0,0 @@
-let add_custom_machinary_coalgen = (event, active, debug) => {
-    if (!active) return;
-
-    event.recipes.custommachinery.custom_machine("mod_journey:coal_gen", 200)
-        .requireItem("#minecraft:coals")
-        .produceEnergy(1000)
-}
