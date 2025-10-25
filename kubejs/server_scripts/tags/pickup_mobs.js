dc725753fefd9af8cd4fcf744a4170af00194d01
zukkl <mrozikrobin@gmail.com>
2025-10-25 11:36:44 +0200

    #104 - disable occultism summond mobs.

--- kubejs/server_scripts/tags/pickup_mobs.js
+++ kubejs/server_scripts/tags/pickup_mobs.js
@@ -11,10 +11,19 @@ let bosses_blacklist = [
     "cataclysm:ender_guardian",
     "cataclysm:netherite_monstrosity",
     "cataclysm:ender_golem",
-    "minecraft:iron_golem"
+    "minecraft:iron_golem",
+    "minecraft:warden",
+    "minecraft:wither"
 ];
 
-// A list on Entity Tags, that will given, to protect it to capture them.
+let occultism_summond_mobs = [
+    "occultism:marid",
+    "occultism:afrit",
+    "occultism:djini",
+    "occultism:foliot"
+]
+
+// A list on Entity Tags, that will given, to protect it to capture them from every Pick-UP Method.
 ServerEvents.tags('entity_type', event => {
 
     bosses_blacklist.forEach(blacklisted_mob => {
@@ -28,6 +37,16 @@ ServerEvents.tags('entity_type', event => {
         event.add('apothic_spawners:blacklisted_from_spawners', blacklisted_mob)
         event.add('c:capturing_not_supported', blacklisted_mob)     //occultism
 
+    });
+
+
+    occultism_summond_mobs.forEach(blacklisted_mob => {
+        event.add('industrialforegoing:mob_duplicator_blacklist', blacklisted_mob)
+        event.add('mob_grinding_utils:no_swab', blacklisted_mob)
+        event.add('mob_grinding_utils:no_spawn', blacklisted_mob)
+        event.add('ars_nouveau:jar_blacklist', blacklisted_mob)
+        event.add('ars_nouveau:animal_summon_blacklist', blacklisted_mob)
+        event.add('apothic_spawners:blacklisted_from_spawners', blacklisted_mob)
     });
 
 })
