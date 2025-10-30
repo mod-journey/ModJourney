dedb875bbb4a37bf6e84e7a0c210f3d421ee70a8
zukkl <mrozikrobin@gmail.com>
2025-10-30 18:08:49 +0100

    add new ore script to function mimik

--- kubejs/server_scripts/main_recipes.js
+++ kubejs/server_scripts/main_recipes.js
@@ -100,23 +100,8 @@ if (events_server) {
         }
 
         if (replace_ores) {
-            event.remove({ output: "#ore:remove" })
-
-            change_gears(event)
-            change_plates(event)
-            change_rods(event)
-
-            ores_replace_immersive(event);
-            ores_replace_laser(event);
-            ores_replace_mekanism_main(event);
-            ores_replace_ore_duplication(event);
-            ores_replace_thermal_thermal(event);
-            ores_replace_thermal_centrifuge(event);
-            ores_replace_thermal_packing(event);
-            ores_replace_thermal_pulverizer(event);
-            ores_replace_thermal_smelter(event);
-            ores_replace_adastra(event);
-            ores_remove(event);
+            fixAlloyRecipes(event, true)
+            changeRecipeToATO(event, true)
         }
 
         if (replace_event) {
@@ -125,7 +110,7 @@ if (events_server) {
             replace_stellaris_steel(event, true);
             replace_stellaris_uran(event, true);
         }
-        fixAlloyRecipes(event, true)
+
     })
 };
 
