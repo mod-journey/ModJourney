315676a63c320687eb0d84d292ff0163ba37efb6
zukkl <mrozikrobin@gmail.com>
2025-11-02 18:19:09 +0100

    allow higher tier  >= Prudentium to Ultimine

--- kubejs/server_scripts/tags/ftb_tags/tools/scythe.js
+++ kubejs/server_scripts/tags/ftb_tags/tools/scythe.js
@@ -2,9 +2,9 @@ let tools_scythe = {
     excluded: [
         //`mysticalagriculture:diamond_scythe`,
         `mysticalagriculture:inferium_scythe`,
-        `mysticalagriculture:prudentium_scythe`,
-        `mysticalagriculture:tertium_scythe`,
-        `mysticalagriculture:imperium_scythe`,
+        //`mysticalagriculture:prudentium_scythe`,
+        //`mysticalagriculture:tertium_scythe`,
+        //`mysticalagriculture:imperium_scythe`,
         //`mysticalagriculture:supremium_scythe`,
         //`mysticalagriculture:awakened_supremium_scythe`,
 
