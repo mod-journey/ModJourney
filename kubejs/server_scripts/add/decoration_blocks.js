db767b5bb1f39f2666889a07a6559eb62161c668
Radon8472 <develop@radon-software.net>
2025-11-14 13:03:59 +0100

    Add recipe for undergarden ashen deepturf plant

--- kubejs/server_scripts/add/decoration_blocks.js
+++ kubejs/server_scripts/add/decoration_blocks.js
@@ -11,4 +11,5 @@ function add_decoration_blocks(event, active) {
 
     // Add recipe as blasting (not .smelting()) to avoid the recipe to be picked up by mekanism and enderIO machines
     event.blasting('undergarden:ashen_deepturf_block', 'undergarden:deepturf_block').cookingTime(200).xp(3)
+    event.blasting('undergarden:ashen_deepturf', 'undergarden:deepturf').cookingTime(100).xp(0.5)
 }
