354feb48aae19c861e3645edcde7b61f09f8ca7c
zukkl <mrozikrobin@gmail.com>
2025-11-16 05:00:47 +0100

    add staging to dark sacrifice bowl

--- kubejs/server_scripts/gated/occultism.js
+++ kubejs/server_scripts/gated/occultism.js
@@ -4,16 +4,24 @@ let occultism_gated = (event, active, debug) => {
 
     //edit the sacrificial_bowl. #earlygame
     event.shaped(Item.of('occultism:sacrificial_bowl', 1), [
-            'BAB',
-            'BBB'
-        ], {
-            A: mjConfig.stages.stage1.magic.early,
-            B: 'occultism:otherstone'
-        }).id('occultism:crafting/sacrificial_bowl')
+        'BAB',
+        'BBB'
+    ], {
+        A: mjConfig.stages.stage1.magic.early,
+        B: 'occultism:otherstone'
+    }).id('occultism:crafting/sacrificial_bowl')
+
+    event.shaped(Item.of('occultism:dark_sacrificial_bowl', 1), [
+        'BAB',
+        'BBB'
+    ], {
+        A: mjConfig.stages.stage1.magic.early,
+        B: 'occultism:otherrock'
+    }).id('occultism:crafting/dark_sacrificial_bowl')
 
     //edit the magic_lamp_empty. #midgame
     replacer.replaceInputByPosition(
-        {output: 'occultism:magic_lamp_empty'},
+        { output: 'occultism:magic_lamp_empty' },
         0, 0,
         { 'item': mjConfig.stages.stage1.magic.mid }
     )
@@ -27,8 +35,8 @@ let occultism_gated = (event, active, debug) => {
         "type": "minecraft:crafting_shaped",
         "key": {
             "y": { "item": "occultism:book_of_binding_empty" },
-            "x": { "tag": "c:dyes/green"},
-            "z": {"item": mjConfig.stages.stage1.magic.end}
+            "x": { "tag": "c:dyes/green" },
+            "z": { "item": mjConfig.stages.stage1.magic.end }
         },
         "pattern": [
             "zxz",
