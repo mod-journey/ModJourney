3b56f8f0f1c654826542475f000d6a92a44b368a
Radon8472 <develop@radon-software.net>
2025-11-18 11:56:59 +0100

    Make stellaris sand usable in sand recipes

--- kubejs/server_scripts/tags/item_stellaris.js
+++ kubejs/server_scripts/tags/item_stellaris.js
@@ -6,10 +6,21 @@ let item_stellaris = (event, active, debug) => {
         'stellaris:raw_uranium'
     ];
 
+    let sand = [
+        'stellaris:mars_sand',
+        'stellaris:moon_sand',
+    ];
+
     uran.forEach(uran_tag => {
         event.removeAllTagsFrom(uran_tag);
         event.add("mod_journey:enriched/uran", uran_tag);
     });
 
     event.removeAllTagsFrom("stellaris:steel_block")
+
+    sand.forEach(sand_block => {
+        event.add('minecraft:sand', sand_block);
+        event.add('c:sands', sand_block);
+        event.add('minecraft:smelts_to_glass', sand_block);
+    });
 }
