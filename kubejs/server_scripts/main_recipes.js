f885e0c679b14ba4edc2eba8c2ff99ddb126e433
zukkl <mrozikrobin@gmail.com>
2025-11-24 03:14:45 +0100

    add seed Planter V2 and recrafting from V1

--- kubejs/server_scripts/main_recipes.js
+++ kubejs/server_scripts/main_recipes.js
@@ -73,6 +73,7 @@ if (events_server) {
 
         if (customMachine_event) {
             add_custom_machinary_seedplanter(event, true)
+            add_custom_machinary_seedplanter_v2(event, true)
             add_custom_machinary_treeplanter(event, true)
             add_custom_machinary_metalpress(event, true)
             add_custom_machinary_brine_converter(event, true)
